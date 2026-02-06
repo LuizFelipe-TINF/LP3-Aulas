ded6f18242728021b5421d382ea199ef3a6ed85c
LuizFelipe-TINF <lfrsilv@gmail.com>
2026-02-06 11:32:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<h1>LP3 Aula 1</h1>
+
+
+<?php
+
+$nome = "Luiz";
+
+echo "Hello World" . $nome . "<br>";
+echo "Hello World $nome <br>";
+
+
+$n1 = 9;
+$n2 = 3;
+
+$soma = $n1 + $n2;
+$mult = $n1 * $n2;
+$div = $n1 / $n2;
+$sub = $n1 - $n2;
+
+
+
+
+echo "A soma de 6 + 3 é $soma, a multiplicação é $mult, a divisão é $div e a subtração é $sub";
+
+
+
+
+?>
