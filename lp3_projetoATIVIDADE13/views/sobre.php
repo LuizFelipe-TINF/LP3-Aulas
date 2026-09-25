bb5177a5c71eb8a9be434b6d61c614bf64fd132c
LuizFelipe-TINF <lfrsilv@gmail.com>
2026-09-25 09:16:24 -0300

    Add files via upload

---
+++ lp3_projetoATIVIDADE13/views/sobre.php
@@ -0,0 +1,10 @@
+<?php include __DIR__ . '/layout/header.php'; ?>
+
+<div class="card">
+    <h1>Bem-vindo ao Curso de PHP Orientado a Objetos</h1>
+    <p>Esta aplicação demonstra a arquitetura MVC com Front Controller e URLs Amigáveis rodando a partir da subpasta <code>/lp3_projeto</code>.</p>
+    <br>
+    <h1>pagina sobre empresa</h1>
+</div>
+
+<?php include __DIR__ . '/layout/footer.php'; ?>
