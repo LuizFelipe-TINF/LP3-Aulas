ba140d0956831d2611ef8a84cb81e7d59a6a893f
LuizFelipe-TINF <lfrsilv@gmail.com>
2026-09-25 08:37:57 -0300

    Add files via upload

---
+++ lp3_projeto (APAGAR_PastasIniciais)/views/home.php
@@ -0,0 +1,10 @@
+<?php include __DIR__ . '/layout/header.php'; ?>
+
+<div class="card">
+    <h1>Bem-vindo ao Curso de PHP Orientado a Objetos</h1>
+    <p>Esta aplicação demonstra a arquitetura MVC com Front Controller e URLs Amigáveis rodando a partir da subpasta <code>/lp3_projeto</code>.</p>
+    <br>
+    <a href="/lp3_projeto/usuarios" class="btn btn-primary">Gerenciar Usuários</a>
+</div>
+
+<?php include __DIR__ . '/layout/footer.php'; ?>
